93533d0bdc7936950591a78a8816def9e6882abe
chakabaka1027 <alexanderdcha@gmail.com>
2018-01-18 12:32:13 -0500

    fixed animation inconsistencies, menu button functional, small glitch in quiz scene removed that allowed menu button to appear too early

--- Assets/Scripts/SceneNavigator.cs
+++ Assets/Scripts/SceneNavigator.cs
@@ -2,69 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Networking;
 
 public class SceneNavigator : MonoBehaviour  {
 
+    public SteamVR_TrackedController leftController;
+    public SteamVR_TrackedController rightController;
+
+    void Update(){
+        if(leftController.menuPressed || rightController.menuPressed){
+            LoadNextScene("Menu");
+        }
+    }
     
     public void LoadNextScene(string sceneName){
-        if(sceneName == "Sign In" && FindObjectOfType<LrsCommunicator>() != null){
-            FindObjectOfType<LrsCommunicator>().ReportLogOut();
-            Destroy(FindObjectOfType<LrsCommunicator>().gameObject);
-        }
         SceneManager.LoadScene(sceneName);
-    }
-
-    public void AssignViewType(string viewType){
-        if (viewType == "AR Model") {
+        if(sceneName == "AR Model"){
             FindObjectOfType<LrsCommunicator>().viewType = LrsCommunicator.ViewType.AR;
-        } else if (viewType == "3D Model") {
+        } else if(sceneName == "3D Model"){
             FindObjectOfType<LrsCommunicator>().viewType = LrsCommunicator.ViewType.nonAR;
         } else {
             FindObjectOfType<LrsCommunicator>().viewType = LrsCommunicator.ViewType.other;
-        }    
-    }
-
-    public void ReturnHome(string sceneName){
-        if (GameObject.Find("LobbyManager") != null) {
-            //if(!Network.isServer){
-                Debug.Log("server disconnect");
-                NetworkManager.singleton.StopClient ();
-                NetworkManager.singleton.StopHost ();
- 
-                NetworkLobbyManager.singleton.StopClient ();
-                NetworkLobbyManager.singleton.StopServer ();
-                NetworkServer.DisconnectAll();
-                //NetworkServer.SetAllClientsNotReady();
-
-            //} else if(!Network.isClient){
-            //    Debug.Log("client disconnect");
-            //    NetworkManager.singleton.StopClient ();
-            //    //NetworkManager.singleton.StopHost ();
- 
-            //    NetworkLobbyManager.singleton.StopClient ();
-            //    //NetworkLobbyManager.singleton.StopServer ();
-            //    Network.Disconnect();
-            //}
-
-            if(GameObject.Find("LobbyManager") != null){
-                Destroy(GameObject.Find("LobbyManager")); 
-            }
-
-        }
-
-        SceneManager.LoadScene(sceneName);
-       
-        FindObjectOfType<LrsCommunicator>().viewType = LrsCommunicator.ViewType.other;
-        
-
-    }
-
-    public void LoadSoloGame(string sceneName){
-        SceneManager.LoadScene(sceneName);
-
-        if(GameObject.Find("LobbyManager") != null){
-            Destroy(GameObject.Find("LobbyManager")); 
         }
     }
 
